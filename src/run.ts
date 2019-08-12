e9e8b4fb6366f0e67c0df5bd70422c511e73ff79
Trevor Sundberg <tsundberg@s2.systems>
2019-08-12 09:30:22 -0700

    Use require.resolve instead of hardcoded path.

--- src/run.ts
+++ src/run.ts
@@ -35,7 +35,10 @@ yargs.command("run", "Run your code in a browser instance", (yarg) => yarg.optio
   expressApp.set("view engine", "handlebars");
   expressApp.set("views", path.join(__dirname, "../views"));
 
-  expressApp.use("/_codesweets/", express.static(path.resolve(__dirname, "..")));
+  expressApp.use("/_node_modules/:module", (req, res, next) => {
+    const dirPath = path.resolve(require.resolve(req.params.module), "..");
+    return express.static(dirPath)(req, res, next);
+  });
   expressApp.use("/_project/", express.static("."));
   expressApp.get("/_project/_loader", (req, res) => {
     res.render("loader", {
